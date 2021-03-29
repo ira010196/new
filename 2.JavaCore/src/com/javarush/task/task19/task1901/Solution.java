3dab6eebc7f1b62c61b8b974fc08b84fbbf4d35c
Irina Lotsmanenko <ira01.01.96@gmail.com>
2021-03-29 19:31:07 +0400

    I did task 1901

--- 2.JavaCore/src/com/javarush/task/task19/task1901/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1901/Solution.java
@@ -1,9 +1,16 @@
 package com.javarush.task.task19.task1901;
-
 /* 
-TableAdapter
+Измени класс TableAdapter так, чтобы он адаптировал ATable к BTable.
+Метод getHeaderText должен возвращать такую строку "[username] : tablename".
+Пример, "[Amigo] : DashboardTable".
+1. Класс Solution должен содержать public static интерфейс ATable.
+2. Класс Solution должен содержать public static интерфейс BTable.
+3. Класс Solution должен содержать public static класс TableAdapter.
+4. Класс TableAdapter должен реализовывать интерфейс BTable.
+5. Класс TableAdapter должен содержать приватное поле aTable типа ATable.
+6. Класс TableAdapter должен содержать конструктор с параметром ATable.
+7. Класс TableAdapter должен переопределять метод getHeaderText согласно заданию.
 */
-
 public class Solution {
     public static void main(String[] args) {
         //это пример вывода
@@ -23,8 +30,17 @@ public class Solution {
         System.out.println(table.getHeaderText());
     }
 
-    public static class TableAdapter {
+    public static class TableAdapter implements BTable{
+        private ATable aTable;
 
+        public TableAdapter(ATable aTable) {
+            this.aTable = aTable;
+        }
+
+        @Override
+        public String getHeaderText() {
+            return "["+aTable.getCurrentUserName()+"]" + " : " + aTable.getTableName();
+        }
     }
 
     public interface ATable {
