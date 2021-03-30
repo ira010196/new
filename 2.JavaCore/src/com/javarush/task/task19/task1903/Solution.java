e918b2134d58219c0f349f85872216c23bd600bd
Irina Lotsmanenko <ira01.01.96@gmail.com>
2021-03-30 17:54:04 +0400

    I did task 1903

--- 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1903/Solution.java
@@ -1,20 +1,104 @@
 package com.javarush.task.task19.task1903;
-
 /* 
 Адаптация нескольких интерфейсов
+Адаптируй IncomeData к Customer и Contact.
+Классом-адаптером является IncomeDataAdapter.
+Инициализируй countries перед началом выполнения программы. Соответствие кода страны и названия:
+UA Ukraine
+RU Russia
+CA Canada
+Дополнить телефонный номер нулями до 10 цифр при необходимости (смотри примеры).
+Обратите внимание на формат вывода фамилии и имени человека.
+1. Класс Solution должен содержать public static поле countries типа Map<String, String>.
+2. В статическом блоке класса Solution инициализируй поле countries тестовыми данными согласно заданию.
+3. Класс IncomeDataAdapter должен реализовывать интерфейсы Customer и Contact.
+4. Класс IncomeDataAdapter должен содержать приватное поле data типа IncomeData.
+5. Класс IncomeDataAdapter должен содержать конструктор с параметром IncomeData.
+6. В классе IncomeDataAdapter реализуй методы интерфейсов Customer и Contact используя подсказки в виде комментариев в интерфейсах.
 */
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class Solution {
     public static Map<String, String> countries = new HashMap<String, String>();
 
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
     public static void main(String[] args) {
+
+        IncomeData incomeData = new IncomeData() {
+            @Override
+            public String getCountryCode() {
+                return "UA";
+            }
+
+            @Override
+            public String getCompany() {
+                return "JavaRush Ltd.";
+            }
+
+            @Override
+            public String getContactFirstName() {
+                return "Ivan";
+            }
+
+            @Override
+            public String getContactLastName() {
+                return "Ivanov";
+            }
+
+            @Override
+            public int getCountryPhoneCode() {
+                return 38;
+            }
+
+            @Override
+            public int getPhoneNumber() {
+                return 501234567;
+            }
+        };
+        IncomeDataAdapter adapter = new IncomeDataAdapter(incomeData);
+        System.out.println(adapter.getCompanyName());
+        System.out.println(adapter.getCountryName());
+        System.out.println(adapter.getName());
+        System.out.println(adapter.getPhoneNumber());
+
 
     }
 
-    public static class IncomeDataAdapter {
+    public static class IncomeDataAdapter implements Customer, Contact{
+        private IncomeData data;
+
+        public IncomeDataAdapter(IncomeData data) {
+            this.data = data;
+        }
+
+        @Override
+        public String getCompanyName() {
+            return data.getCompany();
+        }
+
+        @Override
+        public String getCountryName() {
+            return countries.get(data.getCountryCode());
+        }
+
+        @Override
+        public String getName() {
+            return data.getContactLastName()+", "+data.getContactFirstName();
+        }
+
+        @Override
+        public String getPhoneNumber() {
+                String s = String.format("%010d",data.getPhoneNumber());
+
+                return String.format("+%d(%s)%s-%s-%s", data.getCountryPhoneCode(), s.substring(0,3), s.substring(3,6), s.substring(6,8), s.substring(8, 10));
+
+        }
     }
 
 
