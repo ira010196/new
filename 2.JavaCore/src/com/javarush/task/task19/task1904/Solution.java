9a997730ac3e92a2add8ccef35545d4864ba3917
Irina Lotsmanenko <ira01.01.96@gmail.com>
2021-03-30 19:30:44 +0400

    I did task 1904

--- 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1904/Solution.java
@@ -1,22 +1,64 @@
 package com.javarush.task.task19.task1904;
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
-/* 
-И еще один адаптер
+/*
+Адаптировать Scanner к PersonScanner.
+Классом-адаптером является PersonScannerAdapter.
+В классе адаптере создать приватное финальное поле Scanner fileScanner. Поле инициализировать в конструкторе с одним аргументом типа Scanner.
+Данные в файле хранятся в следующем виде:
+Иванов Иван Иванович 31 12 1950
+Петров Петр Петрович 31 12 1957
+В файле хранится большое количество людей, данные одного человека находятся в одной строке. Метод read() должен читать данные только одного человека.
+1. PersonScanner должен быть интерфейсом.
+2. Класс PersonScannerAdapter должен реализовывать интерфейс PersonScanner.
+3. Класс PersonScannerAdapter должен содержать приватное поле fileScanner типа Scanner.
+4. Класс PersonScannerAdapter должен содержать конструктор с параметром Scanner.
+5. Метод close() класса PersonScannerAdapter должен делегировать полномочие такому же методу fileScanner.
+6. Метод read() класса PersonScannerAdapter должен читать строку с файла, парсить её, и возвращать данные только одного человека, в виде объекта класса Person.
 */
-
 public class Solution {
 
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileName = "C:\\text.txt.txt";
+        Scanner scanner = new Scanner(new FileReader(fileName));
+
 
     }
 
-    public static class PersonScannerAdapter {
+    public static class PersonScannerAdapter implements PersonScanner{
+        private final Scanner fileScanner;
 
+        public PersonScannerAdapter(Scanner fileScanner) {
+            this.fileScanner = fileScanner;
+        }
+
+        @Override
+        public Person read() throws IOException, ParseException {
+            String line = fileScanner.nextLine();
+            String[] all = line.split(" ");
+            String lN = all[0];
+            String n = all[1];
+            String mN = all[2];
+            String day = all[3]+" "+all[4]+" "+all[5];
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy", Locale.ENGLISH);
+            Date date = dateFormat.parse(day);
+            Person person = new Person(n, mN, lN, date);
+            //System.out.println(person);
+
+            return person;
+        }
+
+        @Override
+        public void close() throws IOException {
+            fileScanner.close();
+
+        }
     }
 }
