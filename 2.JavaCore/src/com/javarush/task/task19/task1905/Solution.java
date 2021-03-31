ee7479e077d2f6661aeacffcad5cb286878ea7fc
Irina Lotsmanenko <ira01.01.96@gmail.com>
2021-03-31 13:06:59 +0400

    I did task 1905

--- 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1905/Solution.java
@@ -1,25 +1,109 @@
 package com.javarush.task.task19.task1905;
-
 import java.util.HashMap;
 import java.util.Map;
-
 /* 
 Закрепляем адаптер
+Адаптировать Customer и Contact к RowItem.
+Классом-адаптером является DataAdapter.
+Инициализируй countries перед началом выполнения программы. Соответствие кода страны и названия:
+UA Ukraine
+RU Russia
+CA Canada
+1. Класс Solution должен содержать public static поле countries типа Map<String, String>.
+2. В статическом блоке класса Solution инициализируй поле countries тестовыми данными согласно заданию.
+3. Класс Solution должен содержать интерфейс RowItem.
+4. Класс Solution должен содержать интерфейс Contact.
+5. Класс Solution должен содержать интерфейс Customer.
+6. Класс DataAdapter должен реализовывать интерфейс RowItem.
+7. Класс DataAdapter должен содержать два приватных поля: customer типа Customer и contact Contact.
+8. Класс DataAdapter должен содержать конструктор с параметрами (Customer customer, Contact contact), который инициализирует поля contact и customer.
+9. В классе DataAdapter реализуй методы интерфейса RowItem используя подсказки в виде комментариев в интерфейсах.
 */
-
 public class Solution {
     public static Map<String, String> countries = new HashMap<String, String>();
 
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
     public static void main(String[] args) {
+        Customer customerNew = new Customer() {
+            @Override
+            public String getCompanyName() {
+                return "JavaRush Ltd.";
+            }
+
+            @Override
+            public String getCountryName() {
+                return "Ukraine";
+            }
+        };
+        Contact contactNew = new Contact() {
+            @Override
+            public String getName() {
+                return "Ivanov, Ivan";
+            }
+
+            @Override
+            public String getPhoneNumber() {
+                return "+380(50)123-45-67";
+            }
+        };
+        DataAdapter adapter = new DataAdapter(customerNew, contactNew);
+        System.out.println(adapter.getCountryCode());
+        System.out.println(adapter.getCompany());
+        System.out.println(adapter.getContactFirstName());
+        System.out.println(adapter.getContactLastName());
+        System.out.println(adapter.getDialString());
 
     }
 
-    public static class DataAdapter {
+    public static class DataAdapter implements RowItem{
+        private Customer customer;
+        private Contact contact;
+
         public DataAdapter(Customer customer, Contact contact) {
+            this.contact = contact;
+            this.customer = customer;
+        }
+
+        @Override
+        public String getCountryCode() {
+            for (Map.Entry<String, String> entry: countries.entrySet()) {
+                if (customer.getCountryName().equals(entry.getValue())){
+                    return entry.getKey();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        public String getCompany() {
+            return customer.getCompanyName();
+        }
+
+        @Override
+        public String getContactFirstName() {
+            String[]all = contact.getName().split(", ");
+            return all[1];
+        }
+
+        @Override
+        public String getContactLastName() {
+            String[]all = contact.getName().split(", ");
+            return all[0];
+        }
+
+        @Override
+        public String getDialString() {
+            String num = contact.getPhoneNumber().replaceAll("[^0-9]", "");
+            return String.format("callto://+%s", num);
         }
     }
 
-    public static interface RowItem {
+    public static interface RowItem{
         String getCountryCode();        //For example: UA
 
         String getCompany();            //For example: JavaRush Ltd.
