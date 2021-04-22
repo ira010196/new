d8d0d33bd51ab560de24ca7fdaad602a0b50d0d5
Irina Lotsmanenko <ira01.01.96@gmail.com>
2021-04-22 18:31:51 +0400

    It`s made task 20.01-20.05

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -1,24 +1,35 @@
 package com.javarush.task.task20.task2003;
-
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-/* 
+/*
 Знакомство с properties
+Метод main() считывает имя файла с консоли и заполняет runtimeStorage данными из файла.
+В методах save() и load() реализуй логику записи в файл и чтения из файла для карты runtimeStorage.
+Файл должен быть в формате .properties. Комментарии в файле можно игнорировать.
+Про .properties почитать тут - http://ru.wikipedia.org/wiki/.properties
+1. Метод save() должен сохранять runtimeStorage в параметр outputStream.
+2. Метод load() должен восстанавливать состояние runtimeStorage из параметра inputStream.
 */
-
 public class Solution {
 
     public static Map<String, String> runtimeStorage = new HashMap<>();
 
     public static void save(OutputStream outputStream) throws Exception {
-        //напишите тут ваш код
+        Properties properties = new Properties();
+        properties.putAll(runtimeStorage);
+        properties.store(outputStream,null);
     }
 
     public static void load(InputStream inputStream) throws IOException {
-        //напишите тут ваш код
+        Properties properties = new Properties();
+        properties.load(inputStream);
+        for (Map.Entry<Object, Object> entry: properties.entrySet()) {
+            runtimeStorage.put(entry.getKey().toString(), entry.getValue().toString());
+        }//напишите тут ваш код
+
     }
 
     public static void main(String[] args) {
